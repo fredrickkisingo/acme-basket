3b615e95f062a068eb22b1db65cf3fe5ae3ebf50
Fredrick Kisingo <fredrickkisingo@gmail.com>
2025-10-01 03:38:55 +0300

    [fix]:asserted one can add  a single product and calculate total with delivery.

--- src/Basket.php
+++ src/Basket.php
@@ -2,10 +2,16 @@
 
 class Basket {
     private array $catalogue ;
+    private array $deliveryRules ;
+    private array $items = [];
+    private array $offers = [];
 
-    public function __construct(array $catalogue)
+    public function __construct(array $catalogue,array $deliveryRules,array  $items = [],$offers = [])
     {
         $this->catalogue = $catalogue;
+        $this->deliveryRules = $deliveryRules;
+        $this->items = $items;
+        $this->offers = $offers;
     }
     public function add(string $productCode)
     {
@@ -13,5 +19,40 @@ class Basket {
             throw new InvalidArgumentException("Product $productCode not found in catalogue");
         }
         return $this->items[] = $productCode;
+    }
+
+    public function total(){
+        //Return total basket cost taking into acc delivery and offer rules
+        $subtotal = $this->calculateSubTotal();
+        $discount = $this->applyOffers();
+        $delivery = $this->calculateDeliveryFee($subtotal-$discount);
+
+        return round($subtotal+$delivery-$discount,2);
+    }
+
+    private function calculateSubTotal(){
+        $subtotal = 0.0;
+        foreach($this->items as $item){
+            $subtotal += $this->catalogue[$item]['price'];
+        }
+        return $subtotal;
+    }
+    private function applyOffers(): float
+    {
+        //orders
+        $discount = 0.0;
+        foreach($this->offers as $offer){
+            $discount += $offer->apply($this);
+        }
+        return $discount;
+    }
+    private function calculateDeliveryFee(float $afterDiscount){
+        foreach($this->deliveryRules as $rule){
+            if($afterDiscount >= $rule['threshold']){
+                print_r("Delivery fee is $rule[cost]");
+                return $rule['cost'];
+            }
+        }
+        return 0.0;
     }
 }
--- tests/BasketTest.php
+++ tests/BasketTest.php
@@ -14,13 +14,18 @@ class BasketTest extends TestCase
             'G01' => ['name' => 'Green Widget', 'price' => 24.95],
             'B01' => ['name' => 'Blue Widget', 'price' => 7.95],
         ];
+        $this->deliveryRules = [
+            ['threshold' => 90, 'cost' => 0.0],
+            ['threshold' => 50, 'cost' => 2.95],
+            ['threshold' => 0, 'cost' => 4.95],
+        ];
     }
 
     /** @test */
 
     public function it_can_add_a_single_product_and_calculate_total_with_delivery()
     {
-        $basket = new Basket($this->catalogue);
+        $basket = new Basket($this->catalogue,$this->deliveryRules);
 
         $basket->add('B01');
         $basket->add('G01');
@@ -29,7 +34,7 @@ class BasketTest extends TestCase
     }
 
     public function it_applies_red_widget_offer_when_two_red_widgets_added(){
-        $basket = new Basket($this->catalogue);
+        $basket = new Basket($this->catalogue,$this->deliveryRules);
         $basket->add('R01');
         $basket->add('R01');
         $this->assertEquals(49.43, $basket->total());
